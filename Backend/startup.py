21fb707e015496a575e31b9f8cb0da6373c756bb
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-06 10:04:53 +1000

    Add: Connect and test connection to database

--- Backend/startup.py
+++ Backend/startup.py
@@ -1,5 +1,11 @@
+from pymongo.errors import OperationFailure
+
 from DataIntegration import handbook_reader
 from DataIntegration import visualizer
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 
 def handbook_reader_and_unit_map_visualizer_demo():
@@ -12,5 +18,26 @@ def handbook_reader_and_unit_map_visualizer_demo():
     visualizer.draw_unit_network(unit_network, visible_edges)
 
 
+def mongodb_test():
+    # Load secrets into environment variables
+    load_dotenv("Secrets/mongodb_api.env")
+
+    # Get MongoDB database URI. This will result in an error if Backups/Secrets/mongodb_api.env has not been configured
+    api_key = os.environ.get("MONGODB_API_KEY")
+    api_secret = os.environ.get("MONGODB_API_SECRET")
+    uri = api_key.replace("<password>", api_secret)
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except OperationFailure as e:
+        # todo: handle authentication errors
+        raise
+
+
 if __name__ == "__main__":
-    handbook_reader_and_unit_map_visualizer_demo()
+    mongodb_test()
