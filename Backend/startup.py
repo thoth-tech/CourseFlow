29dee7404fcf56b35916b971e62b5dddee49fb2f
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-11 13:40:24 +1000

    Ref: Move db connection code to persistence layer

--- Backend/startup.py
+++ Backend/startup.py
@@ -1,11 +1,6 @@
-from pymongo.errors import OperationFailure
-
+from Backend.Persistence.connect import mongodb_connect
 from DataIntegration import handbook_reader
 from DataIntegration import visualizer
-import os
-from dotenv import load_dotenv
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
 
 
 def handbook_reader_and_unit_map_visualizer_demo():
@@ -19,26 +14,7 @@ def handbook_reader_and_unit_map_visualizer_demo():
 
 
 def mongodb_test():
-    # Load secrets into environment variables
-    load_dotenv("Secrets/mongodb_api.env")
-
-    # Get MongoDB database URI. This will result in an error if Backups/Secrets/mongodb_api.env has not been configured
-    api_key = os.environ.get("MONGODB_API_KEY")
-    api_secret = os.environ.get("MONGODB_API_SECRET")
-    uri = api_key.replace("<password>", api_secret)
-
-    # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
-
-    # Send a ping to confirm a successful connection
-    try:
-        client.admin.command('ping')
-    except OperationFailure as e:
-        # todo: handle authentication errors
-        raise
-
-    # todo: All interactions with the database such as this belongs in a persistence layer module
-    db = client["development"]
+    db = mongodb_connect()
     unit_collection = db["unit"]
     # todo: debug only: clear collection before continuing
     print("Clearing Unit collection")
