c22f1d0eb33ad6dfa39db2dcfad57db20c3b7606
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-16 17:01:53 +1000

    Add: Unit model

---
+++ Backend/Models/Unit.cs
@@ -0,0 +1,22 @@
+﻿namespace CourseFlow.Backend.Models
+{
+    public class Unit : IUnit
+    {
+        private static int id = 0;
+        public Unit()
+        {
+            Code = $"DEFAULT_CODE_{id}";
+            Title = $"DEFAULT_TITLE_{id}";
+            Description = "";
+            Constraints = new List<IConstraint>();
+            IsDiscontinued = false;
+            id++;
+        }
+
+        public string Code { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<IConstraint> Constraints { get; set; }
+        public bool IsDiscontinued { get; set; }
+    }
+}
