ea4cf07710dd7aa9595812b8843fda6263a760df
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-16 16:28:54 +1000

    Add: Database connection utilities
    Create IUnitRepository.cs

---
+++ Backend/Persistence/IUnitRepository.cs
@@ -0,0 +1,13 @@
+﻿using CourseFlow.Backend.Models;
+
+namespace CourseFlow.Backend.Persistence
+{
+    public interface IUnitRepository
+    {
+        void AddUnit(IUnit unit);
+        void AddMultipleUnits(IEnumerable<IUnit> units);
+        IUnit GetUnitByCode(string unit_code);
+        void UpdateUnit(string unit_code, IUnit unit);
+        void DeleteUnit(string unit_code);
+    }
+}
