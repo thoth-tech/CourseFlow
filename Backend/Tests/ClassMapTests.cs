1fd4e1dffcbd37f9eb7ee350c63e43117740df87
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-17 23:49:14 +1000

    Tst: Unit serializer test

---
+++ Backend/Tests/ClassMapTests.cs
@@ -0,0 +1,25 @@
+using CourseFlow.Backend;
+using CourseFlow.Backend.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+
+namespace CourseFlow_Tests;
+
+[TestClass]
+public class ClassMapTests
+{
+    [TestMethod]
+    public void serialize_unit_to_bson_test()
+    {
+        CourseFlowStartup.RegisterBsonClassMaps();
+
+        Unit unit = new Unit();
+        unit.Code = "TST123";
+        unit.Title = "TEST_UNIT";
+        unit.Description = "description ...";
+
+        string document = unit.ToBsonDocument().ToString();
+
+        Assert.Equals(document, "");
+    }
+}
