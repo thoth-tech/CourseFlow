45120983c6cb54a7497b53e70e822479ae057aeb
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-18 16:29:24 +1000

    Add: Stream implementation
    Used to describe a course/major/minor/specialization etc

---
+++ Backend/Main/Models/Stream.cs
@@ -0,0 +1,12 @@
+﻿using CourseFlow.Backend.Models;
+using CourseFlow.Backend.Models.Constraints;
+
+namespace CourseFlow.Models
+{
+    public class Stream : IStream
+    {
+        public string Code { get; set; }
+        public string HandbookUrl { get; set; }
+        public IEnumerable<IConstraint> Constraints { get; set; }
+    }
+}
