49c82f3eca70fe5d64beb1ce5e18ff220163d6e0
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-18 16:18:20 +1000

    Add: Unit constructor for known initial values
    The blank no parameters constructor is needed for the de-serializer but this is easier to work with in tests.

--- Backend/Main/Models/Unit.cs
+++ Backend/Main/Models/Unit.cs
@@ -6,20 +6,22 @@ namespace CourseFlow.Backend.Models
     public class Unit : IUnit
     {
         private static int id = 0;
-        public Unit()
-        {
-            Code = $"DEFAULT_CODE_{id}";
-            Title = $"DEFAULT_TITLE_{id}";
-            Description = "";
-            Constraints = new List<IConstraint>();
-            IsDiscontinued = false;
-            id++;
-        }
-
         public string Code { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public IEnumerable<IConstraint> Constraints { get; set; }
         public bool IsDiscontinued { get; set; }
+
+        public Unit() : this($"DEFAULT_CODE_{id}", $"DEFAULT_TITLE_{id}", "", new List<IConstraint>(), false) { }
+
+        public Unit(string code, string title, string description, IEnumerable<IConstraint> constraints, bool isDiscontinued)
+        {
+            Code = code;
+            Title = title;
+            Description = description;
+            Constraints = constraints;
+            IsDiscontinued = isDiscontinued;
+            id++;
+        }
     }
 }
