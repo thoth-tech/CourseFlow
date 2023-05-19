b31ce56f79fcbdce160cf8f192cc9809a372d126
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-05-19 18:43:36 +1000

    Add: Constraint discriminator convention

--- Backend/Main/Utils.cs
+++ Backend/Main/Utils.cs
@@ -2,7 +2,9 @@
 using CourseFlow.Backend.Models;
 using CourseFlow.Backend.Models.Constraints;
 using MongoDB.Bson;
+using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using SharpCompress.Common;
@@ -10,6 +12,73 @@ using System;
 
 namespace CourseFlow.Backend
 {
+    public class ConstraintDiscriminatorConvention : IDiscriminatorConvention
+    {
+        private Dictionary<string, Type> discriminatorToTypeMap = new();
+        private Dictionary<Type, string> typeToDiscriminatorMap = new();
+        public string ElementName => "type";
+        private IDiscriminatorConvention defaultConvention;
+
+        public ConstraintDiscriminatorConvention()
+        {
+            defaultConvention = new HierarchicalDiscriminatorConvention(ElementName);
+        }
+
+        public ConstraintDiscriminatorConvention RegisterDiscriminator<T>(string discriminatorName) where T : AbstractConstraint
+        { 
+            discriminatorToTypeMap[discriminatorName] = typeof(T);
+            typeToDiscriminatorMap[typeof(T)] = discriminatorName;
+            BsonSerializer.RegisterDiscriminatorConvention(typeof(T), this);
+
+            return this;
+        }
+
+        public void RemoveDiscriminator(string discriminatorName)
+        {
+            typeToDiscriminatorMap.Remove(discriminatorToTypeMap[discriminatorName]);
+            discriminatorToTypeMap.Remove(discriminatorName);
+        }        
+        
+        public void RemoveDiscriminator(Type discriminatorType)
+        {
+            discriminatorToTypeMap.Remove(typeToDiscriminatorMap[discriminatorType]);
+            typeToDiscriminatorMap.Remove(discriminatorType);
+        }
+
+        public new Type GetActualType(IBsonReader bsonReader, Type nominalType)
+        {
+            var bookmark = bsonReader.GetBookmark();
+            bsonReader.ReadStartDocument();
+            if (bsonReader.FindElement(ElementName))
+            {
+                var documentDiscriminatorValue = bsonReader.ReadString();
+                // Map the discriminator value to the corresponding concrete type
+                foreach (string constraintDiscriminatorValue in discriminatorToTypeMap.Keys)
+                {
+                    if (documentDiscriminatorValue == constraintDiscriminatorValue)
+                    {
+                        bsonReader.ReturnToBookmark(bookmark);
+                        return discriminatorToTypeMap[constraintDiscriminatorValue];
+                    }
+                }
+
+                throw new NotImplementedException($"Discriminator value {documentDiscriminatorValue} does not belong to any registered constraint. Register constraints with discriminators using RegisterDiscriminator()");
+            }
+
+            bsonReader.ReturnToBookmark(bookmark);
+            return defaultConvention.GetActualType(bsonReader, nominalType);
+        }
+
+        public BsonValue GetDiscriminator(Type nominalType, Type actualType)
+        {
+            if (!typeToDiscriminatorMap.Keys.Contains(actualType))
+            { 
+                throw new NotImplementedException($"Constraint {actualType} is not registered to a discriminator. Register constraints with discriminators using RegisterDiscriminator()");
+            }
+            return typeToDiscriminatorMap[actualType];
+        }
+    }
+
     public static class Utils
     {
         public static void RegisterBsonClassMaps()
