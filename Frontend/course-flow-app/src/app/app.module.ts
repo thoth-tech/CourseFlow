fad746780e393b3cca7d2c155e72f62f4c95f49d
chris-marktopher <cmarktopher@gmail.com>
2023-07-30 21:12:27 +1000

    Added discovery cluster map

--- Frontend/course-flow-app/src/app/app.module.ts
+++ Frontend/course-flow-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiscoveryPageComponent } from './discovery-page/discovery-page.component';
 import { DiscoveryForceDirectedMapComponent } from './discovery-force-directed-map/discovery-force-directed-map.component';
+import { DiscoveryClusterMapComponent } from './discovery-cluster-map/discovery-cluster-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiscoveryPageComponent,
-    DiscoveryForceDirectedMapComponent
+    DiscoveryForceDirectedMapComponent,
+    DiscoveryClusterMapComponent
   ],
   imports: [
     BrowserModule,
