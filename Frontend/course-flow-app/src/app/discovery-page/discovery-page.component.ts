9b3c8da13eb5efa5d9f6768914107bb088475321
chris-marktopher <cmarktopher@gmail.com>
2023-07-30 21:07:43 +1000

    Added simple discovery display switching.

--- Frontend/course-flow-app/src/app/discovery-page/discovery-page.component.ts
+++ Frontend/course-flow-app/src/app/discovery-page/discovery-page.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./discovery-page.component.css']
 })
 export class DiscoveryPageComponent {
-
+  
+  // The map type to display
+  mapType = "force-directed";
 }
