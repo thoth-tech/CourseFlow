2726e8a3456e6330b3349e79dd45b00478cad59f
chris-marktopher <cmarktopher@gmail.com>
2023-07-31 20:54:28 +1000

    Added basic responsive svg extents.

--- Frontend/course-flow-app/src/app/discovery-force-directed-map/discovery-force-directed-map.component.ts
+++ Frontend/course-flow-app/src/app/discovery-force-directed-map/discovery-force-directed-map.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import * as d3 from "d3";
 
 @Component({
@@ -7,19 +7,88 @@ import * as d3 from "d3";
   styleUrls: ['./discovery-force-directed-map.component.css']
 })
 export class DiscoveryForceDirectedMapComponent {
-  // Width and height of the svg element - will need to figure out how to make this responsive.
-  private width = 1200;
-  private height = 1000
+  
+  // SVG element data
+  svgElementData = {
+    "small": {
+      "width": 300,
+      "height": 300,
+      "nodeRadius": 6
+    },
+    "medium": {
+      "width": 500,
+      "height": 500,
+      "nodeRadius": 10
+    },
+    "large": {
+      "width": 800,
+      "height": 800,
+      "nodeRadius": 15
+    },
+    "xLarge": {
+      "width": 1200,
+      "height": 1200,
+      "nodeRadius": 20
+    }
+  }
+
+  currentSvgElementData = this.svgElementData.small;
 
   // Zoom extents
   private minZoom = 0;
   private maxZoom = 10;
 
+
+  /**
+   * Called after component is created.
+   */
   ngOnInit(): void {
-    this.createDiscoveryForceDirectedMap();
+
+    this.preCreateDiscoveryForceDirectedMap();
   }
 
+  /**
+   * Callback for when the window is resized.
+   */
+  @HostListener("window:resize", [])
+  private onWindowResized() {
+
+    this.preCreateDiscoveryForceDirectedMap();
+
+  }
+
+  /**
+   * Handles pre-configurations prior to calling createDiscoveryForceDirectedMap().
+   */
+  preCreateDiscoveryForceDirectedMap() : void {
+    
+    let width = window.innerWidth;
+
+    if (width > 1300){
+      this.currentSvgElementData = this.svgElementData.xLarge;
+      this.createDiscoveryForceDirectedMap();
+    }
+    else if (width <= 1300 && width > 800){
+      this.currentSvgElementData = this.svgElementData.large;
+      this.createDiscoveryForceDirectedMap();
+    }
+    else if (width <= 800 && width > 400) {
+      this.currentSvgElementData = this.svgElementData.medium;
+      this.createDiscoveryForceDirectedMap();
+    }
+    else if (width <= 400) {
+      this.currentSvgElementData = this.svgElementData.small;
+      this.createDiscoveryForceDirectedMap();
+    }
+  }
+
+  /**
+   * Core logic to create the force directed discovery map.
+   */
   createDiscoveryForceDirectedMap(): void {
+
+    // In case we already have a svg element (can happen on window resize events)
+    d3.select("svg").remove();
 
     // Create the svg canvas
     let baseSvgCanvas = this.createBaseSvgCanvas();
@@ -33,9 +102,9 @@ export class DiscoveryForceDirectedMapComponent {
 
     // TODO Remove once zoom is tested.
     zoomableGroup.append("circle")
-      .attr("cx", this.width / 2)
-      .attr("cy", this.height / 2)
-      .attr("r", 20)
+      .attr("cx", this.currentSvgElementData.width / 2)
+      .attr("cy", this.currentSvgElementData.height / 2)
+      .attr("r", this.currentSvgElementData.nodeRadius)
       .style("fill", "white")
   }
 
@@ -47,8 +116,8 @@ export class DiscoveryForceDirectedMapComponent {
     
     let svg = d3.select("div#discoveryForceDirectedMap")
     .append("svg")
-    .attr("width", this.width)
-    .attr("height", this.height)
+    .attr("width", this.currentSvgElementData.width)
+    .attr("height", this.currentSvgElementData.height)
     .style("background", "#232224")
     .style("border-radius", "20px")
 
