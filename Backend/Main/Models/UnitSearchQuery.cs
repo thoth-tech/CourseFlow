fb543ad548f4559765187299de3f24fb3f82ac49
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-08-16 15:30:17 +1000

    add: Model for unit search queries

---
+++ Backend/Main/Models/UnitSearchQuery.cs
@@ -0,0 +1,12 @@
+﻿using CourseFlow.Backend.Models.Constraints;
+
+namespace CourseFlow.Backend.Models;
+
+public class UnitSearchQuery
+{
+    public string Code { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public IEnumerable<string> Keywords { get; set; } = new List<string>();
+    public int Level { get; set; }
+}
+ 
