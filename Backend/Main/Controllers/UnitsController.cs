204ee441fa2d21136983a849450b26e90291d3ca
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-08-16 13:14:52 +1000

    add: Endpoints to get unit data from the backend

---
+++ Backend/Main/Controllers/UnitsController.cs
@@ -0,0 +1,41 @@
+﻿using CourseFlow.Backend.Models;
+using CourseFlow.Backend.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseFlow.Backend.Controllers;
+
+[ApiController]
+[Route("api/units")]
+public class UnitsController : ControllerBase
+{
+    private readonly IUnitRepository _unitsRepo;
+    public UnitsController(IUnitRepository unitsRepo)
+    { 
+        _unitsRepo = unitsRepo;
+    }
+
+    [HttpGet("{code}")]
+    public IActionResult GetUnitByCode(string code)
+    { 
+        IUnit? unit = _unitsRepo.GetUnitByCode(code);
+
+        if (unit == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(unit);
+    }
+
+    [HttpGet()]
+    public IActionResult GetAllUnits()
+    {
+        return Ok(_unitsRepo.GetAllUnits());
+    }
+
+    [HttpGet("search/{code}")]
+    public IActionResult SearchUnitsByCode(string code)
+    {
+        return Ok(_unitsRepo.SearchUnitsByCode(code));
+    }
+}
