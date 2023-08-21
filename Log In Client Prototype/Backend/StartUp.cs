90198a37c11c82859fabc48d8c65844ffca1eefb
SoVinhou <101162260+SoVinhou@users.noreply.github.com>
2023-08-21 16:16:07 +1000

    Log In Client prototype/demo
    This demo file contains both backend (using C#) and frontend (using typescript and angular). The demo is almost done, once it is finalized we can integrate it into the main code quite easily.

---
+++ Log In Client Prototype/Backend/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyProjectName
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add session middleware
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
+            // Other service registrations
+            // ...
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Use session middleware
+            app.UseSession();
+
+            // Other middleware registrations
+            // ...
+        }
+    }
+}
