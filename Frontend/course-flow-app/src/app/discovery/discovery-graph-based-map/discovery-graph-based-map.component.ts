b546b1c2a13ebe4d1b1ecb183e6255474bd279cb
chris-marktopher <cmarktopher@gmail.com>
2023-09-07 20:35:27 +1000

    Selecting nodes will highlight associated links

--- Frontend/course-flow-app/src/app/discovery/discovery-graph-based-map/discovery-graph-based-map.component.ts
+++ Frontend/course-flow-app/src/app/discovery/discovery-graph-based-map/discovery-graph-based-map.component.ts
@@ -2,7 +2,7 @@
 import { Component, Input, Inject } from '@angular/core';
 
 // Interface Imports
-import { IDiscoveryDataServiceInjector, IDiscoveryDataService, IDiscoveryData } from 'src/app/interfaces/discoveryInterfaces';
+import { IDiscoveryDataServiceInjector, IDiscoveryDataService, IDiscoveryData, IDiscoveryNodeData, IDiscoveryLinkData } from 'src/app/interfaces/discoveryInterfaces';
 
 // Enum Imports
 import { EDiscoveryGroupUnitsBy } from "../../enum/discoveryEnums"
@@ -23,9 +23,18 @@ export class DiscoveryGraphBasedMapComponent {
   // Canvas, node and link visuals.
   baseSvgCanvasElement: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> | null = null;
   zoomableGroupElement: d3.Selection<SVGGElement, unknown, HTMLElement, any> | null = null;
+  renderedNodeGroup: d3.Selection<SVGGElement | d3.BaseType, IDiscoveryNodeData, SVGGElement, unknown> | null = null;
+  renderedLinks: d3.Selection<SVGGElement | d3.BaseType, IDiscoveryLinkData, SVGGElement, unknown> | null = null;
 
-  // Zoom Level
+  // Graph Properties.
+  width: number = 1920;
+  height: number = 1920;
   zoomLevel: number = 0.5;
+
+  // Edge Properties
+  linkWidth: number = 0.1;
+  linkOpacity: number = 0.1;
+  linkColor: string = "black";
    
   constructor(@Inject(IDiscoveryDataServiceInjector) private discoveryDataService: IDiscoveryDataService) {}
 
@@ -39,9 +48,28 @@ export class DiscoveryGraphBasedMapComponent {
   }
 
   /**
+   * Finds a node by id.
+   * @param id Id of the node.
+   * @returns Found node.
+   */
+  getNodeById(id: string): IDiscoveryNodeData {
+
+    let foundNode: IDiscoveryNodeData | null = null;
+  
+    if (this.discoveryData.nodeData) {
+
+      foundNode = this.discoveryData.nodeData.find((node) => node.id === id) as IDiscoveryNodeData;
+    }
+      
+    return foundNode as IDiscoveryNodeData;
+  }
+
+  /**
    * Create the discovery map.
    */
   createDiscoveryMap(): void {
+
+    d3.select("div#graphBasedMap").select("svg").remove();
 
     this.baseSvgCanvasElement = this.createBaseCanvas();
 
@@ -49,7 +77,11 @@ export class DiscoveryGraphBasedMapComponent {
 
     this.initializeZoom();
 
+    this.renderLinks();
+
     this.renderNodes();
+
+
   }
 
   /**
@@ -61,10 +93,10 @@ export class DiscoveryGraphBasedMapComponent {
     return d3
       .select("div#graphBasedMap")
       .append("svg")
-      .attr("width", 800)
-      .attr("height", 800)
+      .attr("width", this.width)
+      .attr("height", this.height)
       .style("background-color", "white")
-      .style("border-radius", 20);
+      .style("border-radius", "20px");
   }
 
   /**
@@ -86,29 +118,33 @@ export class DiscoveryGraphBasedMapComponent {
     if (this.baseSvgCanvasElement) {
 
         this.baseSvgCanvasElement.call(zoomBehaviour.transform, 
-          d3.zoomIdentity.translate(500 / 2, 500 / 2)
+          d3.zoomIdentity.translate(this.width / 4, this.height / 4)
                          .scale(this.zoomLevel))
                          .call(zoomBehaviour);
     }
   }
 
+  /**
+   * Render nodes.
+   */
   renderNodes(): void {
 
     if (this.zoomableGroupElement) {
 
       // Create the node group.
-      let renderedNodeGroup = this.zoomableGroupElement.selectAll("node")
-                                                       .data(this.discoveryData.nodeData)
-                                                       .join("g")
-                                                       .attr("transform", nodeData => `translate(${(nodeData as any).x * 800}, ${(nodeData as any).y * 800})`)
+      this.renderedNodeGroup = this.zoomableGroupElement.selectAll("node")
+        .data(this.discoveryData.nodeData)
+        .join("g")
+        .attr("transform", nodeData => `translate(${(nodeData as any).x * this.width}, ${(nodeData as any).y * this.height})`)
+        .on("click", (event, nodeData) => this.handleOnNodeClicked(nodeData))
 
       // Append a circle shape to the node group.
-      renderedNodeGroup.append('circle')
-                       .attr("fill", "blue")
-                       .attr("r", 2)
+      this.renderedNodeGroup.append('circle')
+        .attr("fill", "blue")
+        .attr("r", 2)
 
       // Append text to the node group.
-      renderedNodeGroup.append("text")
+      this.renderedNodeGroup.append("text")
         .text((nodeData) => nodeData.label)  
         .style("font-size", 50)
         .style("visibility", (nodeData) => {
@@ -122,5 +158,80 @@ export class DiscoveryGraphBasedMapComponent {
           return visibility
         })
       }
+  }
+
+  /**
+   * Render links.
+   */
+  renderLinks(): void {
+
+    if (this.zoomableGroupElement) {
+
+      this.renderedLinks = this.zoomableGroupElement.append("g")
+        .selectAll("line")
+        .data(this.discoveryData.linkData)
+        .join("line")
+        .attr("x1", link => (this.getNodeById(link.source).x as number) * this.width)
+        .attr("y1", link => (this.getNodeById(link.source).y as number) * this.height)
+        .attr("x2", link => (this.getNodeById(link.target).x as number) * this.width)
+        .attr("y2", link => (this.getNodeById(link.target).y as number) * this.height)
+        .attr("stroke", this.linkColor)
+        .attr("stroke-width", this.linkWidth)
+        .attr("stroke-opacity", this.linkOpacity)
+    } 
+  }
+
+  /**
+   * Handles logic when a node is clicked.
+   */
+  handleOnNodeClicked(nodeClicked: IDiscoveryNodeData): void {
+
+    this.toggleLinks(nodeClicked.id);
+  }
+
+
+  /**
+   * Modify required link properties to highlight related links to node.
+   * @param originNodeId Origin node id.
+   * @param targetNodeId Target node id.
+   */
+  toggleLinks(nodeId: string): void {
+    
+    if (this.renderedLinks) {
+
+      this.renderedLinks
+        .attr("stroke", link => {
+
+          let color = this.linkColor;
+          
+          if (nodeId === link.source || nodeId === link.target) {
+            color = "blue"
+          }
+
+          return color;
+        })
+        .attr("stroke-opacity", link => {
+
+          let oapcity = this.linkOpacity;
+          
+          if (nodeId === link.source || nodeId === link.target) {
+            
+            oapcity = 1;
+          }
+
+          return oapcity;
+        })
+        .attr("stroke-width", link => {
+
+          let width = this.linkWidth;
+          
+          if (nodeId === link.source || nodeId === link.target) {
+            
+            width = 1;
+          }
+
+          return width;
+        })
+    }
   }
 }
--- Frontend/course-flow-app/src/app/discovery/discovery-services/json-file-discovery-data.service.ts
+++ Frontend/course-flow-app/src/app/discovery/discovery-services/json-file-discovery-data.service.ts
@@ -8,14 +8,23 @@ import { EDiscoveryGroupUnitsBy } from 'src/app/enum/discoveryEnums';
 import { IDiscoveryDataService, IDiscoveryData, IDiscoveryNodeData, IDiscoveryLinkData } from 'src/app/interfaces/discoveryInterfaces';
 
 // JSON Data Imports
-import  * as facultyData from "src/data/facultyData.json";
+import facultyData from "src/data/facultyData.json";
 
 @Injectable({
   providedIn: 'root'
 })
 export class JsonFileDiscoveryDataService implements IDiscoveryDataService {
 
-  constructor() {}
+  // Json data
+  discoveryJsonData: Record<EDiscoveryGroupUnitsBy, object> = {
+    [EDiscoveryGroupUnitsBy.faculty]: facultyData,
+    [EDiscoveryGroupUnitsBy.course]: facultyData,
+    [EDiscoveryGroupUnitsBy.related_units]: facultyData,
+  };
+
+  constructor() {
+
+  }
 
   /**
    * Get discovery data which will consist of data for nodes and edges/links. 
@@ -23,39 +32,70 @@ export class JsonFileDiscoveryDataService implements IDiscoveryDataService {
    */
   getDiscoveryData(groupUnitsByQuery: EDiscoveryGroupUnitsBy): IDiscoveryData {
     
-    let discoveryNodeData: IDiscoveryData = {} as IDiscoveryData;
+    let discoveryData = {} as IDiscoveryData;
 
-    switch (groupUnitsByQuery) {
+    this.processNodes(discoveryData);
+    this.processLinks(discoveryData);
 
-      case EDiscoveryGroupUnitsBy.faculty:
-        
-        // Add the nodes.
-        discoveryNodeData.nodeData = facultyData as IDiscoveryNodeData[];
-
-        // Figure out the links and add them.
-        let linkData = [] as IDiscoveryLinkData[];
-
-        for (let i = 0; i < discoveryNodeData.nodeData.length; i++) {
-
-          let node = discoveryNodeData.nodeData[i];
-          
-          linkData.push({
-            id: node.id + node.inConnections[0],
-            source: node.inConnections[0],
-            target: node.id
-          })
-
-          discoveryNodeData.linkData = linkData;
-        }
-        
-        break;
-    
-      default:
-
-        break;
-    }
-
-    return discoveryNodeData
+    return discoveryData
   }
 
+  /**
+   * Process data and add in nodes.
+   * @param discoveryData Discovery data containing data for nodes and links.
+   */
+  processNodes(discoveryData: IDiscoveryData) : void {
+    
+    discoveryData.nodeData = facultyData as IDiscoveryNodeData[];
+
+  }
+
+  /**
+   * Process data and add in links/edges.
+   * @param discoveryData Discovery data containing data for nodes and links.
+   */
+  processLinks(discoveryData: IDiscoveryData): void {
+
+    let linkData = [] as IDiscoveryLinkData[];
+
+    // Go through each node, check all connection arrays, and create the links.
+    for (let i = 0; i < discoveryData.nodeData.length; i++) {
+
+      let node = discoveryData.nodeData[i];
+
+      this.addLinkUsingConnections(node, linkData, node.inConnections);
+      this.addLinkUsingConnections(node, linkData, node.outConnections);
+      this.addLinkUsingConnections(node, linkData, node.coConnections);
+      this.addLinkUsingConnections(node, linkData, node.restrictedConnections);
+    }
+
+    discoveryData.linkData = linkData;
+  }
+
+  /**
+   * Use the connections from the json data to create the links.
+   * @param node Current node being processed.
+   * @param linkData Link data array we are 
+   * @param nodeConnectionArray Array of node ids which will be used to create the links.
+   */
+  addLinkUsingConnections(node: IDiscoveryNodeData, linkData: IDiscoveryLinkData[], nodeConnectionArray: string[]): void {
+
+    for (let i = 0; i < nodeConnectionArray.length; i++) {
+        
+      // Since we can have connections going in and out from a node, we will likely come accross them multiple times.
+      // Hence, we check the two possible id combinations to ensure we don't double up on links.
+      // Link Ids will always be added from the current node id to the id within the connection array.
+      let foundLink: IDiscoveryLinkData | undefined = linkData.find((link) => link.id === node.id + nodeConnectionArray[i] || link.id === nodeConnectionArray[i] + node.id);
+
+      if (foundLink !== undefined) {
+        continue;
+      }
+
+      linkData.push({
+        id: node.id + nodeConnectionArray[i],
+        source: node.id,
+        target: nodeConnectionArray[i]
+      })  
+    }
+  }
 }
