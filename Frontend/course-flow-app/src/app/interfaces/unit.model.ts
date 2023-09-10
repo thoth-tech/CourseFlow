a63b2962ef221a581f7157518857939f79d364ee
wmvoon <wm.voon@gmail.com>
2023-09-11 00:28:00 +1000

    Update changes for html file to link with json file
    - Create a link for URL to be able to read in a href tag.
    - Update html to read json file
    - Update required properties for typescript file to read

--- Frontend/course-flow-app/src/app/interfaces/unit.model.ts
+++ Frontend/course-flow-app/src/app/interfaces/unit.model.ts
@@ -1,16 +1,31 @@
 export interface Unit {
-    Code: string;
-    Name: string;
-    Type: string;
-    Content: string;
-    CreditPoints: string;
-    Prerequisite: string;
-    ULOs: string[];
-    GLOs: string[];
-    Assessment: {
-      Description: string;
-      Weight: number;
-      WeekDue: string;
-    }[];
-  }
-  
+  Code: string;
+  Name: string;
+  Type: string;
+  Overview: String;
+  UnitChairT1: String;
+  UnitChairT2: String;
+  UnitChairT3: String;
+  Melbourne: String;
+  Geelong: String;
+  Online: String;
+  CreditPoints: string;
+  Availability: String;
+  Prerequisite: string;
+  Hurdle: String;
+  OnCampusLearning1: String;
+  OnCampusLearning2: String;
+  OnCampusLearning3: String;
+  CloudLearning1: String;
+  CloudLearning2: String;
+  CloudLearning3: String;
+  Content: string;
+  Note: String;
+  ULOs: string[];
+  GLOs: string[];
+  Assessment: {
+    Description: string;
+    Weight: number;
+    WeekDue: string;
+  }[];
+}
--- Frontend/course-flow-app/src/app/unit-detail-dialog/unit-detail-dialog.component.ts
+++ Frontend/course-flow-app/src/app/unit-detail-dialog/unit-detail-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Unit } from '../interfaces/unit.model';
 
@@ -7,12 +7,18 @@ import { Unit } from '../interfaces/unit.model';
   templateUrl: `./unit-detail-dialog.component.html`,
   styleUrls: ['./unit-detail-dialog.component.css']
 })
-export class UnitDetailDialogComponent {
+export class UnitDetailDialogComponent implements OnInit {
+  Unit_Link: any;
+
   constructor(@Inject(MAT_DIALOG_DATA)
   public data: Unit,
   public dialogRef: MatDialogRef<UnitDetailDialogComponent>) { }
 
   isDropdownOpen: boolean = false;
+
+  ngOnInit(): void {
+    this.Unit_Link = this.data;
+  }
 
   toggleDropdown(): void {
     this.isDropdownOpen = !this.isDropdownOpen;
