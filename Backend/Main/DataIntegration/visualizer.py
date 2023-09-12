56e23b8c94f5a0499c46f0e5532c8de88363ac7b
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-09-12 18:45:31 +1000

    ref: Rework layout algorithm to be fully OOP based

--- Backend/Main/DataIntegration/visualizer.py
+++ Backend/Main/DataIntegration/visualizer.py
@@ -1,5 +1,6 @@
 import math
 import json
+from itertools import chain
 from typing import Dict, Tuple, List, Set, Iterable
 from threading import Thread
 from abc import ABC, abstractmethod
@@ -47,171 +48,180 @@ def unit_distance_metric(unit_1: Unit, unit_2: Unit) -> float:
 
 
 class UnitNetwork(ABC):
-    def __init__(self, unit_distances: Dict[Tuple[str, str], float], graph: nx.DiGraph):
+    def __init__(self, unit_distances: Dict[Tuple[str, str], float]):
         self.unit_distances = unit_distances
-        self._graph = graph
-        self.node_positions = {}
+        self.unit_positions = {}
 
-    @property
-    def graph(self):
-        return self._graph
+class Node(ABC):
+    def __init__(self, network: UnitNetwork, depth: int=0, graph_label: int=0):
+        self.network = network
+        self.depth = depth
+        self.graph_label = graph_label
 
-
-class UnitNetworkNode(ABC, UnitNetwork):
-    """Represents nodes of the root cluster."""
-    def __init__(self, unit_distances: Dict[Tuple[str, str], float], graph: nx.DiGraph, label_in_root_cluster: int):
-        super.__init__(self, unit_distances, graph)
-        self.label_in_root_cluster = label_in_root_cluster
-
-    def distance(self, other_cluster):
-        """Returns the average distance between two root cluster nodes"""
+    def distance(self, other_node):
+        """Returns the average distance between two nodes"""
         total_distance = 0
         n_edges = 0
         for code_a in self:
-            for code_b in other_cluster:
+            for code_b in other_node:
                 if code_a == code_b:
                     continue
                 edge = (code_a, code_b)
-                if edge in self.unit_distances.keys():
-                    total_distance += self.unit_distances[edge]
+                if edge in self.network.unit_distances.keys():
+                    total_distance += self.network.unit_distances[edge]
                     n_edges += 1
 
         return total_distance / n_edges
 
     @abstractmethod
     def __iter__(self):
+        """Returns an iterable of unit codes belonging to the node"""
         pass
 
 
-class UnitNetworkRootNode(UnitNetwork):
-    """Used to determine the overall layout of each cluster in the network layout diagram"""
-    def __init__(self, unit_distances: Dict[Tuple[str, str], float]):
-        super().__init__(unit_distances, nx.DiGraph())
-        self._clusters = {}
-        self._distances_calculated = False
+class LeafNode(Node):
 
-    def build(self):
-        """Call this after all nodes have been added to calculate the distances"""
-
-        # Add edges between nodes, with the distance between each node as the edge weight
-        for node_1 in self._clusters.values():
-            for node_2 in self._clusters.values():
-                if node_1 == node_2:
-                    continue
-                self._graph.add_edge(node_1.label, node_2.label, weight=node_1.distance(node_2))
-        self._distances_calculated = True
-
-    def __getitem__(self, cluster_label):
-        return self._clusters[cluster_label]
-
-    def __setitem__(self, cluster_label, cluster: UnitNetworkNode):
-        self._clusters[cluster_label] = cluster
-
-    @property
-    def graph(self):
-        assert self._distances_calculated, ".build() must be called before accessing the graph to calculate the edges"
-        return self._graph
-
-
-class UnitNetworkNoiseNode(UnitNetworkNode):
-    """Placeholder for single-unit clusters"""
-    def __init__(self, unit_distances: Dict[Tuple[str, str], float], unit_code: str, label_in_root_cluster: int):
-        graph = nx.DiGraph()
-        graph.add_node(unit_code)
-        super().__init__(unit_distances, graph, label_in_root_cluster)
+    def __init__(self, network: UnitNetwork, graph_label: int, depth: int, unit_code: str):
+        super().__init__(network, graph_label, depth)
         self.unit_code = unit_code
 
     def __iter__(self):
         yield self.unit_code
 
 
+class ClusterNode(Node):
+    # Hyperparameters
+    epsilon = 0.25
+    min_samples_in_epsilon_neighborhood = 3
+    scale = 10
 
-class UnitNetworkClusterNode(UnitNetworkNode):
-    """Used to determine the layout of units within a cluster"""
-    def __init__(self, cluster_units: Dict[str, Unit], label_in_root_cluster: int, unit_distances: Dict[Tuple[str, str], float]):
-        unit_codes = cluster_units.keys()
-        self.unit_codes = unit_codes
+    def __init__(self, network: UnitNetwork, depth: int, graph_label: int, units: Dict[str, Unit]):
+        super().__init__(network, depth, graph_label)
+        self.units = units
+        self.layout_graph = nx.DiGraph()
+        self.n_clusters = 0
+        self.n_noise = 0
+        self.sub_clusters = []
+        self.sub_cluster_processing_threads = []
+        self.leaf_nodes = []
+        self.child_nodes_created = False
+        self.layout_complete = False
+        self.layouts_applied = False
+        self.node_positions = {}
 
-        self.distances_between_units_in_cluster = {}
-        for code_a in unit_codes:
-            for code_b in unit_codes:
-                if code_a == code_b:
+    def identify_and_break_into_sub_clusters(self):
+        # Convert remaining nodes to leaf nodes and return if condition met
+        # todo: Adjust this condition as necessary
+        if self.depth == 2:
+            for leaf_node_label, node_unit_code in enumerate(self.units.keys()):
+                self.layout_graph.add_node(leaf_node_label)
+                self.leaf_nodes.append(LeafNode(self.network, leaf_node_label, self.depth + 1, node_unit_code))
+            self.child_nodes_created = True
+
+            return
+
+        # Create an adjacency matrix with the distances between each unit as the edge weights
+        distance_matrix = np.ones((len(self.units), len(self.units)), dtype=float)
+        np.fill_diagonal(distance_matrix, 0)
+        unit_codes = sorted(self.units.keys())
+        unit_codes_to_index = {code: i for i, code in enumerate(unit_codes)}
+        for (from_unit, to_unit), distance in self.network.unit_distances.items():
+            from_index = unit_codes_to_index[from_unit]
+            to_index = unit_codes_to_index[to_unit]
+            distance_matrix[from_index, to_index] = distance
+
+        # Identify which cluster each unit belongs to based on their distance to each other
+        db = DBSCAN(eps=ClusterNode.epsilon,
+                    min_samples=ClusterNode.min_samples_in_epsilon_neighborhood,
+                    metric='precomputed').fit(distance_matrix)
+        labels = db.labels_
+        n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+        n_noise_nodes = list(labels).count(-1)
+
+        for cluster_node_label in range(n_clusters):
+            # Create a cluster node containing the units identified by the clustering algorithm
+            indices_of_units_in_cluster, = np.where(labels == cluster_node_label)
+            units_in_cluster = {unit_codes[i]: self.units[unit_codes[i]] for i in indices_of_units_in_cluster}
+            cluster_node = ClusterNode(self.network, self.depth + 1, cluster_node_label, units_in_cluster)
+
+            # Add the cluster node to the layout graph and this cluster
+            self.sub_clusters.append(cluster_node)
+            self.layout_graph.add_node(cluster_node_label)
+
+            # Process the sub-cluster node in a separate thread
+            thread = Thread(target=cluster_node.build)
+            thread.start()
+            self.sub_cluster_processing_threads.append(thread)
+
+        # Add the leaf nodes to the layout graph and this cluster
+        indices_of_noise_units = np.where(labels == -1)
+        for i in range(n_noise_nodes):
+            leaf_node_label = -i - 1
+            node_unit_code = unit_codes[indices_of_noise_units[i]]
+            self.layout_graph.add_node(leaf_node_label)
+            self.leaf_nodes.append(LeafNode(self.network, leaf_node_label, self.depth + 1, node_unit_code))
+
+        self.child_nodes_created = True
+
+    def build_layout(self):
+        assert self.child_nodes_created, "identify_and_break_into_sub_clusters() must be completed first"
+
+        # Add edges between nodes, with the distance between each node as the edge weight
+        for node_1 in self:
+            for node_2 in self:
+                if node_1 == node_2:
                     continue
-                edge = (code_a, code_b)
-                if edge in unit_distances:
-                    self.distances_between_units_in_cluster[edge] = unit_distances[edge]
+                self.layout_graph.add_edge(node_1.graph_label, node_2.graph_label, weight=node_1.distance(node_2))
 
-        graph = create_unit_graph(cluster_units, self.distances_between_units_in_cluster)
-        super().__init__(unit_distances, graph, label_in_root_cluster)
+        # Use the Kamada-Kawai network layout algorithm on the nodes to determine the layout
+        self.node_positions = nx.kamada_kawai_layout(self.layout_graph, scale=ClusterNode.scale)
+
+        self.layout_complete = True
+
+    def apply_layout_to_sub_clusters(self):
+        assert self.layout_complete, "build_layout() must be completed first"
+        # fixme: Layout will probably be wrong the first time around, come back to this and adjust as necessary
+
+        # Apply this cluster's layout to leaf nodes
+        for node in self.leaf_nodes:
+            node: LeafNode
+
+            x, y = self.node_positions[node.unit_code]
+            x *= ClusterNode.scale
+            y *= ClusterNode.scale
+            self.network.unit_positions[node.unit_code] = (x, y)
+
+        # Apply this cluster's layout to sub-clusters
+        for sub_cluster_processing_thread, sub_cluster in zip(self.sub_cluster_processing_threads, self.sub_clusters):
+            sub_cluster_processing_thread: Thread
+            sub_cluster: ClusterNode
+
+            # Wait for the sub-cluster to finish processing before applying this cluster's layout to it
+            sub_cluster_processing_thread.join()
+
+            for unit_code, (x, y) in sub_cluster.node_positions.items():
+                final_x, final_y = self.network.unit_positions[unit_code]
+                final_x = final_x + x * ClusterNode.scale
+                final_y = final_y + y * ClusterNode.scale
+                self.network.unit_positions[unit_code] = (final_x, final_y)
+
+        self.layouts_applied = True
+
+    def build(self):
+        self.identify_and_break_into_sub_clusters()
+        self.build_layout()
+        self.apply_layout_to_sub_clusters()
 
     def __iter__(self):
-        return self.unit_codes.__iter__()
-
-
-def build_cluster_network_layout(cluster: UnitNetworkClusterNode, scale: int=1):
-    cluster.node_positions = nx.kamada_kawai_layout(cluster.graph, scale=scale)
+        return chain(self.sub_clusters.__iter__(), self.leaf_nodes.__iter__())
 
 
 def build_unit_network_layout(units: Dict[str, Unit], unit_distances: Dict[Tuple[str, str], float]) -> Dict[str, Tuple[float, float]]:
-    ###################
-    # Hyperparameters #
-    ###################
-    epsilon = 0.25 # Smaller values result in smaller and tighter clusters, larger values create larger and more inclusive clusters
-    min_samples_in_epsilon_neighborhood = 3
-    root_layout_scale = 10
+    unit_network = UnitNetwork(unit_distances)
+    root_node = ClusterNode(unit_network, units=units, depth=0, graph_label=0)
+    root_node.build()
 
-
-    # Create an adjacency matrix with the distances between each unit as the edge weights
-    distance_matrix = np.ones((len(units), len(units)), dtype=float)
-    np.fill_diagonal(distance_matrix, 0)
-    unit_codes = sorted(units.keys())
-    unit_codes_to_index = {code: i for i, code in enumerate(unit_codes)}
-    for (from_unit, to_unit), distance in unit_distances.items():
-        from_index = unit_codes_to_index[from_unit]
-        to_index = unit_codes_to_index[to_unit]
-        distance_matrix[from_index, to_index] = distance
-
-    # Use calculated distances to determine which clusters to form and which cluster each unit should belong to
-    # todo: Create hyperparameter tuner that minimizes the total number of nodes of all clusters by tuning epsilon
-    #  Also consider the total number of clusters + noise nodes
-    db = DBSCAN(eps=epsilon, min_samples=min_samples_in_epsilon_neighborhood, metric='precomputed').fit(distance_matrix)
-    labels = db.labels_
-    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise = list(labels).count(-1)
-    layout_threads = []
-    unit_network_root_node = UnitNetworkRootNode(unit_distances)
-
-    for label in range(n_clusters):
-        # Create a graph using the nodes that belong to each cluster
-        indices_of_units_in_cluster, = np.where(labels == label)
-        units_in_cluster = {unit_codes[i]: units[unit_codes[i]] for i in indices_of_units_in_cluster}
-
-        # Use the Kamada-Kawai network layout algorithm on the nodes within each cluster in parallel
-        cluster_node = UnitNetworkClusterNode(units_in_cluster, label, unit_distances)
-        unit_network_root_node[label] = cluster_node
-        thread = Thread(target=build_cluster_network_layout, args=(cluster_node,))
-        layout_threads.append(thread)
-        thread.start()
-
-    # Add noise nodes to unit network
-    indices_of_noise_units = np.where(labels == -1)
-    for i in range(n_noise):
-        label = -i - 1  # Noise nodes will have negative labels in the root graph starting from -1
-        noise_unit_code = unit_codes[indices_of_noise_units[i]]
-        unit_network_root_node[label] = UnitNetworkNoiseNode(unit_distances, noise_unit_code, label)
-
-    unit_network_root_node.build()
-
-    # Use Kamada-Kawai on each cluster, treating each cluster itself as a node to determine centroid positions
-    thread = Thread(target=build_cluster_network_layout, args=(unit_network_root_node.graph, root_layout_scale))
-    layout_threads.append(thread)
-    thread.start()
-
-    # Wait for all threads to finish calculating the network layout
-    for thread in layout_threads:
-        thread.join()
-    # todo: Offset the node positions in each cluster with the cluster's centroid
-    pass
+    return root_node.node_positions
 
 
 def draw_unit_network(network: nx.DiGraph, visible_edges: List[Tuple[str, str]], pos: Dict[str, Tuple[float, float]] = None):
