586be0b1995e691fff3489d626fd0f801c95b31a
SoVinhou <101162260+SoVinhou@users.noreply.github.com>
2023-09-18 19:48:49 +1000

    Fix Leaked API

--- Frontend/CourseFlow_Login_Form/src/environments/environment.ts
+++ Frontend/CourseFlow_Login_Form/src/environments/environment.ts
@@ -1,14 +1,15 @@
 /* Environment variables for Application
 change firebase config variables if the owner is changed */
+
 export const environment = {
-    production: false,
-    firebaseConfig: {
-      apiKey: "AIzaSyD5exVJKLke-qTg8M-v57cuGKJy3TpMi1A",
-      authDomain: "courseflow-firebase.firebaseapp.com",
-      projectId: "courseflow-firebase",
-      storageBucket: "courseflow-firebase.appspot.com",
-      messagingSenderId: "396425742820",
-      appId: "1:396425742820:web:d2835c5045d1b000cc5ff1",
-      measurementId: "G-VGDWGNTCN1"
-    }
+  production: false,
+
+  /* Firebase configuration: User can use firebase config keys
+  from the provided CouseFlow Firebase account here.
+
+  firebaseConfig: {
+  }
+  */
+ 
 };
+
