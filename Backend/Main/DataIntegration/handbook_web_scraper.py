5f59b95abf499b9a5629fc9a8d336caf4271de68
Vortrox <39481603+Vortrox@users.noreply.github.com>
2023-09-21 23:12:18 +1000

    ref: Extract webpage downloading and caching code

--- Backend/Main/DataIntegration/handbook_web_scraper.py
+++ Backend/Main/DataIntegration/handbook_web_scraper.py
@@ -11,33 +11,42 @@ class UnitSearchQuery:
         self.year = year
 
 
-def download_unit_lists():
-    """Downloads and saves the unit list webpages for all known faculties"""
-    prettify_documents = True
+def download_and_cache_unit_list_webpage(faculty_code: str, session: requests.Session=None):
+    """
+    Downloads the resulting webpage when searching for all units with the faculty code.
+
+    :param faculty_code: Single character representing a faculty. At Deakin University this is the first letter of a unit code.
+    :param session: An existing Session to Deakin University's website. If None, creates a new Session.
+    """
+
     base_url = "https://www.deakin.edu.au"
     resource = "/current-students-courses/unit-search.php"
     webpage_cache_filepath = pathlib.Path("webpage_cache")
     webpage_search_list_filepath = webpage_cache_filepath / "unit_search_lists"
+    webpage_file_name = f"faculty_{faculty_code}_unit_list.html"
+
+    # Download unit list from university website
+    if session is None:
+        session = requests.Session()
+    response = session.get(base_url + resource, params=UnitSearchQuery(unit_code=faculty_code).__dict__)
+
+    # Parse webpage document
+    soup = BeautifulSoup(response.text, "html.parser")
+    html_document = soup.prettify()
+
+    # Save document to cache
+    with open(webpage_search_list_filepath / webpage_file_name, "w") as fp:
+        fp.write(html_document)
+
+
+def download_unit_lists():
+    """Downloads and saves the unit list webpages for all known faculties"""
     session = requests.Session()
     faculty_codes = ['E', 'A', 'H', 'S', 'M']
 
     for faculty_code in faculty_codes:
         # Search for all units with the matching faculty code and download the search webpage
-        query_params = {
-            "entunit": faculty_code,
-            "entkeyword": None,
-            "year": 2023
-        }
-        response = session.get(base_url + resource, params=query_params)
-
-        # Parse document
-        soup = BeautifulSoup(response.text, "html.parser")
-        html_document = soup.prettify() if prettify_documents else response.text
-
-        # Save document to cache
-        webpage_file_name = f"faculty_{faculty_code}_unit_list.html"
-        with open(webpage_search_list_filepath / webpage_file_name, "w") as fp:
-            fp.write(html_document)
+        download_and_cache_unit_list_webpage(faculty_code, session)
 
 
 if __name__ == "__main__":
